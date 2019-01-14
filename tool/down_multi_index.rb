06b01502c53b0bfb71e9615ff48589c25a1f87a0
eggplants <w10776e8w@yahoo.co.jp>
2019-01-14 15:40:31 +0900

    Update down_multi_index.rb

--- tool/down_multi_index.rb
+++ tool/down_multi_index.rb
@@ -5,14 +5,12 @@ require 'csv'
 def index_htms(board,index)
   thread_url = []
   for j in 0..300
-    source = OpenURI
-    .open_uri(URI.encode("https://tsumanne.net/#{board}/#{index}/#{j.to_s}")).read
+    source = OpenURI.open_uri(URI
+      .encode("https://tsumanne.net/#{board}/#{index}/#{j.to_s}")).read
     puts("Now analysing:#{j+1} page...")
-    an = source
-    .scan(/[a-z]{2}.{1}[a-z]{4}.{1}[0-9]{4}.{1}[0-9]{2}.{1}[0-9]{2}.{1}[0-9]{7}/)
+    an = source.scan(/[a-z]{2}.{1}[a-z]{4}.{1}[0-9]{4}.{1}[0-9]{2}.{1}[0-9]{2}.{1}[0-9]{7}/)
     .uniq.flatten
-    thread_url.push(an) if an.size != 0
-    break if an.size == 0
+    an.size != 0 ? thread_url.push(an) : break
   end
   puts "#{thread_url.size} thread(s) found!"
   return thread_url
@@ -30,16 +28,17 @@ def down_thread(board,index,thread_url)
         fileDir = "#{dirName}#{htm};#{index.join("_")}"
         unless FileTest.exist?(fileDir)
           FileUtils.mkdir_p(fileDir)
-          FileUtils.cp(["../style/th.css", "../style/th.js"], fileDir)
+          FileUtils.cp([File.join(__dir__, "../style/th.css"),
+          File.join(__dir__, "../style/th.js")], fileDir)
         else
           puts "exist!"
         end
-        htmPath = fileDir + "/" + htm
-        htm_src = 'https://tsumanne.net/' + koshiro + "/" + htm
+        htmPath = "#{fileDir}/#{htm}"
+        htm_src = "https://tsumanne.net/#{koshiro}/#{htm}"
         print("Now saving:", htm, "...")
         open(htmPath, 'wb') do |output|#save thread htm file
           begin
-            output.write(io_2source.sub(/src.*js"/, 'src="./th.js"'))
+            output.write(source.sub(/src.*js"/, 'src="./th.js"'))
             output.write(source.gsub(/shift-JIS/, "UTF-8"))
             puts("successful!\n")
           rescue => er
@@ -49,7 +48,7 @@ def down_thread(board,index,thread_url)
         thread_data_0 = source.scan(/[0-9]{10,}\.[a-z]{3,4}/)
         thread_data_1 = source.scan(/[a-z]{2}[0-9]+\.[a-z]{3,4}/).push(thread_data_0).flatten
         thread_data_2 = source.scan(/[0-9]+s\.\w{3,4}/).push(thread_data_1).flatten.uniq
-        thread_data_2.each do |odaie|
+        (source.scan(/[0-9]+s\.\w{3,4}/).push(thread_data_1).flatten.uniq).each do |odaie|
           img_src = "https://tsumanne.net/#{koshiro}/#{odaie}"
           print('Now saving:', odaie, '...')
           fileName = File.basename(img_src)
@@ -63,7 +62,7 @@ def down_thread(board,index,thread_url)
                 }
               rescue => er
                 puts $!
-                CSV.open("./csv/not_found.csv",'a') do |log|
+                CSV.open("../csv/not_found.csv",'a') do |log|
                   log << [htm,odaie,index]
             end;end;end
           else
